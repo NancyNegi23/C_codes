2b77ec0aeaf65ea0a2f912c51081f462a6c763fd
NancyNegi <78430749+NancyNegi23@users.noreply.github.com>
2021-07-15 17:28:45 +0530

    Create Cash.c

---
+++ Cash.c
@@ -0,0 +1,52 @@
+#include <ctype.h>
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+
+// Points assigned to each letter of the alphabet
+int POINTS[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
+
+int compute_score(string word);
+
+int main(void)
+{
+    // Get input words from both players
+    string word1 = get_string("Player 1: ");
+    string word2 = get_string("Player 2: ");
+
+    // Score both words
+    int score1 = compute_score(word1);
+    int score2 = compute_score(word2);
+
+    //Prints the winner
+    if (score1 > score2)
+    {
+        printf("Player 1 wins!\n");
+    }
+    else if (score1 < score2)
+    {
+        printf("Player 2 wins!\n");
+    }
+    else if (score1 == score2)
+    {
+        printf("Tie!\n");
+    }
+}
+
+// function to calculate the score of each player
+int compute_score(string word)
+{
+    int score = 0;
+    for (int i = 0, n = strlen(word); i < n; i++)
+    {
+        if (isupper(word[i])) //check if the input is uppercase
+        {
+            score += POINTS[word[i] - 'A'];
+        }
+        else if (islower(word[i])) // check if the output is lowercase
+        {
+            score += POINTS[word[i] - 'a'];
+        }
+    }
+    return score;
+}
